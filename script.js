d03da4aafb26df03c7b6a55f023d139ccf5eb7af
AliceQiao <alice.qiao@ocsbstudent.ca>
2021-03-22 14:45:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let height = 0
+let base1 = 0
+let base2 = 0
+let number = 0
+
+document.getElementById('button').addEventListener('click', math)
+
+function math () {
+  height = document.getElementById('height').value
+  base1 = document.getElementById('base1').value
+  base2 = document.getElementById('base2').value
+  number = base1/2 * height + base2/2 *height
+  alert(number)
+}
